0fd07bb50882565bb943ba1255e8ac9cd4e26639
Vladislav <111982932+v1ab1@users.noreply.github.com>
2026-02-21 21:12:42 +0300

    ready

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,11 +16,37 @@ MainWindow::MainWindow(QWidget *parent)
         ui->pb2, &QPushButton::clicked,
         this, &MainWindow::on_number_input
         );
+    connect(
+        ui->plusButton, &QPushButton::clicked, this, &MainWindow::on_operation_nput);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_operation_nput() {
+    QPushButton* pb = dynamic_cast<QPushButton*>(sender());
+
+    if (state == State::FirstDigit) {
+        return;
+    }
+
+    if (state == State::InputNumber) {
+        bool ok;
+        double right = ui->leInput->text().toDouble(&ok);
+        pendingOperation = pb->text();
+
+        if (ok) {
+            if (pendingOperation == "+")
+                currentValue = currentValue + right;
+
+            ui->historyLabel->setText(QString::number(currentValue) + pendingOperation);
+            ui->leInput->setText(QString::number(currentValue));
+            state = State::FirstDigit;
+        }
+        return;
+    }
 }
 
 void MainWindow::on_number_input() {
@@ -35,7 +61,19 @@ void MainWindow::on_number_input() {
     ui->leInput->setText(ui->leInput->text() + digit);
 }
 
+void MainWindow::save_value() {
+    bool ok;
+    double value = ui->leInput->text().toDouble(&ok);
+    if (ok) {
+        rememberedValue = value;
+    }
+}
+
 void MainWindow::reset() {
     ui->leInput->setText("0");
     state = State::FirstDigit;
+    currentValue = 0.0;
+    rememberedValue = 0.0;
+    addValue = 0.0;
+    pendingOperation.clear();
 }
